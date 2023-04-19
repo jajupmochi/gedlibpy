b80eaa6cec48216c09424804e57eca6ccc25b108
jajupmochi <jajupmochi@gmail.com>
2023-04-19 18:24:14 +0200

    [Enhancement] Update librariesImport.py to use real path.

--- librariesImport.py
+++ librariesImport.py
@@ -1,5 +1,6 @@
 from ctypes import *
-lib1 = cdll.LoadLibrary('lib/fann/libdoublefann.so')
-lib2 = cdll.LoadLibrary('lib/libsvm.3.22/libsvm.so')
-lib3 = cdll.LoadLibrary('lib/nomad/libnomad.so')
-lib4 = cdll.LoadLibrary('lib/nomad/libsgtelib.so')
+import os
+lib1 = cdll.LoadLibrary(os.path.dirname(os.path.realpath(__file__)) + '/lib/fann/libdoublefann.so')
+lib2 = cdll.LoadLibrary(os.path.dirname(os.path.realpath(__file__)) + '/lib/libsvm.3.22/libsvm.so')
+lib3 = cdll.LoadLibrary(os.path.dirname(os.path.realpath(__file__)) + '/lib/nomad/libnomad.so')
+lib4 = cdll.LoadLibrary(os.path.dirname(os.path.realpath(__file__)) + '/lib/nomad/libsgtelib.so')
